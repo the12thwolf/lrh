15d07114002c574941771e7bd7ea3526e146dc03
the12thwolf <the12thwolf@126.com>
2018-01-30 14:29:21 +0800

    添加变长参数测试

--- lrh-web/src/main/java/com/ww/test/argstest/ArgsTest.java
+++ lrh-web/src/main/java/com/ww/test/argstest/ArgsTest.java
@@ -4,7 +4,10 @@ package com.ww.test.argstest;
  * Created by Administrator on 2018/1/30.
  */
 public class ArgsTest {
-    public int  argsSum(int... values){
+    public static void main(String[] args){
+        System.out.println(argsSum(1,2,3,4,5,6,7,8,9,10));
+    }
+    public static int  argsSum(int... values){
         int sum=0;
         for(int value:values) sum +=value;
         return sum;
